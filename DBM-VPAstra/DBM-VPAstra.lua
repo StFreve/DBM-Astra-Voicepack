c76705ab269a238edaf8066c5b050e9626d34de0
StFreve <Golfik2007@gmail.com>
2022-02-27 17:58:49 +0400

    Register sounds in proper way

--- DBM-VPAstra/DBM-VPAstra.lua
+++ DBM-VPAstra/DBM-VPAstra.lua
@@ -1,8 +1,34 @@
 local LibStub, DBM, DBM_GUI, DBM_OPTION_SPACER = _G["LibStub"], DBM, DBM_GUI, DBM_OPTION_SPACER
+local victorySoundsRegistered, defeatSoundsRegistered, warningSoundsRegistered = false, false, false
 
-do
+function RegisterAstraVicotrySounds() -- Register Victory sounds
+	if victorySoundsRegistered then
+		return
+	end
+
+	-- Make sure EACH entry has a UNIQUE text
+	-- DBM:AddVictorySound("Astra Victory Sound", "Interface\\AddOns\\DBM-VPAstra\\victory.ogg", 4)
+	victorySoundsRegistered = true
+end
+
+function RegisterAstraDefeatSounds() -- Register Defeat sounds
+	if defeatSoundsRegistered then
+		return
+	end
+
+	-- Make sure EACH entry has a UNIQUE text
+	-- DBM:AddDefeatSound("Astra Defeat Sound", "Interface\\AddOns\\DBM-VPAstra\\defeat.ogg", 1)
+	defeatSoundsRegistered = true
+end
+
+function RegisterAstraWarningSounds() -- Register Warning sounds
+	if warningSoundsRegistered then
+		return
+	end
+
 	local LSM = LibStub("LibSharedMedia-3.0")
 
+	-- Make sure EACH entry has a UNIQUE text
 	LSM:Register("sound", "Algalon: Beware! (Russian)", [[Interface\AddOns\DBM-VPAstra\ClassicSupport\UR_Algalon_BHole01.ogg]])
 	LSM:Register("sound", "BB Wolf: Run Away (Russian)", [[Interface\AddOns\DBM-VPAstra\ClassicSupport\HoodWolfTransformPlayer01.ogg]])
 	LSM:Register("sound", "Illidan: Not Prepared (Russian)", [[Interface\AddOns\DBM-VPAstra\ClassicSupport\BLACK_Illidan_04.ogg]])
@@ -10,6 +36,9 @@ do
 	LSM:Register("sound", "Kil'Jaeden: Destruction (Russian)", [[Interface\AddOns\DBM-VPAstra\ClassicSupport\KILJAEDEN02.ogg]])
 	LSM:Register("sound", "Loatheb: I see you (Russian)", [[Interface\AddOns\DBM-VPAstra\ClassicSupport\LOA_NAXX_AGGRO02.ogg]])
 
-	--DBM:AddVictorySound("Astra Victory Sound", "Interface\\AddOns\\DBM-VPAstra\\victory.ogg", 4)
-	--DBM:AddDefeatSound("Astra Defeat Sound", "Interface\\AddOns\\DBM-VPAstra\\defeat.ogg", 1)
+	warningSoundsRegistered = true
+end
+
+do
+	RegisterAstraWarningSounds()
 end
