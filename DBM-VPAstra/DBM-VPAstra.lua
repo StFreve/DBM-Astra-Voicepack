e11ae5367b9158c110ace71797be70bd470525e2
StFreve <Golfik2007@gmail.com>
2022-02-27 16:26:18 +0400

    Prepared Voice Pack to add extra sounds for Victory and Defeat

---
+++ DBM-VPAstra/DBM-VPAstra.lua
@@ -0,0 +1,6 @@
+local DBM = DBM
+
+do
+	--DBM:AddVictorySound("Astra Victory Sound", "Interface\\AddOns\\DBM-VPAstra\\victory.ogg", 4)
+	--DBM:AddDefeatSound("Astra Defeat Sound", "Interface\\AddOns\\DBM-VPAstra\\defeat.ogg", 1)
+end
